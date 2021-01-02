1972fc9587d070a2b11f74503ced8121d64df992
Leon <leon.schaefer@schaefer-it-solutions.com>
2021-01-02 16:38:55 +0100

    deleted placeholder content and added tinytickets logo to Head Component

---
+++ src/components/Head.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <img alt="logo" src="../assets/logo.png">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Head',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
